36d41d7e875079bd0f60b766a8a11ebfee033acf
sujalmh9@gmail.com <sujalmh>
2024-11-08 18:20:06 +0530

    Added extract.py

--- extract.py
+++ extract.py
@@ -2,34 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import time
 import os
+from urllib.parse import unquote, unquote_plus
 
-# Path to your existing Chrome profile
 chrome_profile_path = r"C:\Users\sujal\AppData\Local\Google\Chrome\User Data"
-profile_directory = "Profile 1"  # Replace with the name of your Chrome profile
-
-# Initialize Chrome options
+profile_directory = "Default"
 chrome_options = Options()
-
-# Enable headless mode (run without GUI)
-chrome_options.add_argument("--headless")
-
-# Use the existing Chrome profile
+chrome_options.add_argument(f"--headless")
 chrome_options.add_argument(f"user-data-dir={chrome_profile_path}")  # Path to user data
 chrome_options.add_argument(f"profile-directory={profile_directory}")  # The specific profile you want to load
 
 # Set up WebDriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+def get_skills(profileurl):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get(profileurl)
 
-# Open a website to verify it's working
-driver.get("https://www.google.com")
 
-# Wait for the page to load
-time.sleep(3)
+    elements = driver.find_elements(By.XPATH, '//a[@data-field="skill_page_skill_topic"]')
 
-# Get the title of the page
-print(driver.title)  # Should print "Google"
+    skills = []
+    for element in elements:
+        skill = element.get_attribute("href")
+        skill = ''.join(skill.split('https://www.linkedin.com/search/results/all/?keywords='))
+        skill = skill.replace("&origin=PROFILE_PAGE_SKILL_NAVIGATION","")
+        skill = unquote(skill)
+        skill = skill.replace("+"," ")
+        skills.append(skill)
 
-# Close the browser
-driver.quit()
+    driver.quit()
+    return list(set(skills))
+
+print(get_skills("https://www.linkedin.com/in/sujnankumar/details/skills/"))
